29fed49ad2826cb1a2cc4d1e65486d2ddec91f91
ore0z <me@mattjohn.io>
2023-12-28 12:02:39 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pi-Hole Control
+
+This is a Chrome extension that will enable and disable pi-hole ad blocking. It supports multiple instances and offers a quick way to disable, and re-enable blocking.
+
+You will need an API key for your pi-hole instance. Also note, if you're web interface is running on an alternate port, you must include that in the IP Address field.
---
+++ popup.js
@@ -0,0 +1,102 @@
+document.addEventListener('DOMContentLoaded', function() {
+    displaySavedSettings();
+    document.getElementById('addPiholeButton').addEventListener('click', addNewPihole);
+});
+
+function addNewPihole() {
+    var newAddress = document.getElementById('newPiholeAddress').value.trim();
+    var newKey = document.getElementById('newApiKey').value.trim();
+
+    if (newAddress && newKey) {
+        chrome.storage.local.get('piholeSettings', function(result) {
+            var piholeSettings = result.piholeSettings || [];
+            piholeSettings.push({ address: newAddress, key: newKey });
+
+            chrome.storage.local.set({ 'piholeSettings': piholeSettings }, function() {
+                displaySavedSettings();
+                document.getElementById('newPiholeAddress').value = ''; // Clear the input fields
+                document.getElementById('newApiKey').value = '';
+            });
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    displaySavedSettings();
+    document.getElementById('addPiholeButton').addEventListener('click', addNewPihole);
+    document.getElementById('disableAllPiholeButton').addEventListener('click', disableAllPihole);
+    document.getElementById('enableAllPiholeButton').addEventListener('click', enableAllPihole);
+});
+
+function disableAllPihole() {
+    var selectedDuration = document.getElementById('disableDuration').value;
+    makeApiCallForAllPihole(`disable=${selectedDuration}`);
+}
+function enableAllPihole() {
+    makeApiCallForAllPihole('enable');
+}
+
+function makeApiCallForAllPihole(query) {
+    chrome.storage.local.get('piholeSettings', function(result) {
+        if (result.piholeSettings && result.piholeSettings.length > 0) {
+            result.piholeSettings.forEach(setting => {
+                fetch(`http://${setting.address}/admin/api.php?${query}&auth=${setting.key}`)
+                    .then(response => response.json())
+                    .then(data => console.log(`Pi-hole at ${setting.address} ${query.startsWith('disable') ? 'disabled' : 'enabled'}:`, data))
+                    .catch(error => console.error(`Error ${query.startsWith('disable') ? 'disabling' : 'enabling'} Pi-hole at ${setting.address}:`, error));
+            });
+        } else {
+            console.log('No Pi-hole settings saved.');
+        }
+    });
+}
+
+
+function displaySavedSettings() {
+    chrome.storage.local.get('piholeSettings', function(result) {
+        const settingsContainer = document.getElementById('savedPiholeSettings');
+        settingsContainer.innerHTML = '';
+
+        if (result.piholeSettings && result.piholeSettings.length > 0) {
+            result.piholeSettings.forEach((setting, index) => {
+                // Container div for each setting line
+                const settingLineDiv = document.createElement('div');
+                settingLineDiv.className = 'settingLine';
+
+                // Div for displaying the address
+                const addressDiv = document.createElement('div');
+                addressDiv.textContent = `Address: ${setting.address}`;
+                addressDiv.className = 'addressDiv';
+
+                // Create the delete button
+                var deleteButton = document.createElement('button');
+                deleteButton.className = 'deleteButton';
+                deleteButton.textContent = 'Delete';
+                deleteButton.addEventListener('click', function() {
+                    deleteSetting(index);
+                });
+
+                settingLineDiv.appendChild(addressDiv);
+                settingLineDiv.appendChild(deleteButton);
+                settingsContainer.appendChild(settingLineDiv);
+            });
+        } else {
+            settingsContainer.textContent = 'No saved Pi-hole settings.';
+        }
+    });
+}
+
+
+
+function deleteSetting(index) {
+    chrome.storage.local.get('piholeSettings', function(result) {
+        var piholeSettings = result.piholeSettings || [];
+        if (index >= 0 && index < piholeSettings.length) {
+            piholeSettings.splice(index, 1);
+
+            chrome.storage.local.set({ 'piholeSettings': piholeSettings }, function() {
+                displaySavedSettings();
+            });
+        }
+    });
+}
